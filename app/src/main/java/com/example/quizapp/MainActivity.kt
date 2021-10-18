bd5114734bcef13adc7734308d55a1864020ab0b
Elekes Rebeka <elekes.p.rebeka@student.ms.sapientia.ro>
2021-10-18 18:00:19 +0300

    Contact button

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.quizapp
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.graphics.Color
+import android.media.RingtoneManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
@@ -20,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var playerName: EditText
     private lateinit var startButton: Button
+    private lateinit var contactButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i(TAG,"onCreate()")
         super.onCreate(savedInstanceState)
@@ -53,6 +60,10 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG,"onDestroy()")
     }
 
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the returned Uri
+    }
+
     private fun registerListeners(){
         lateinit var mHandler: Handler
         lateinit var mRunnable: Runnable
@@ -69,11 +80,17 @@ class MainActivity : AppCompatActivity() {
             mHandler = Handler()
             mHandler.postDelayed(mRunnable, 1000)
         }
+
+        contactButton.setOnClickListener{
+            //getContent.launch("contacts/*")
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+            startActivityForResult(intent, 2)
+        }
     }
 
     private fun initializeView(){
         playerName = findViewById(R.id.playerNameInput)
         startButton = findViewById(R.id.startButton)
-
+        contactButton = findViewById(R.id.contactButton)
     }
 }
