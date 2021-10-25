623f81df0d34dd560deff02debec3db037ce5468
Elekes Rebeka <elekes.p.rebeka@student.ms.sapientia.ro>
2021-10-25 22:01:41 +0300

    Navigation created, displayed questions

--- app/src/main/java/com/example/quizapp/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/quizapp/ui/quiz/QuizFragment.kt
@@ -10,10 +10,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.quizapp.R
 import com.example.quizapp.TAG
 import com.example.quizapp.models.Question
+import com.example.quizapp.models.questions
 import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
@@ -32,15 +37,18 @@ class QuizFragment : Fragment() {
     private var param2: String? = null
 
     private lateinit var nextButton: Button
+    private lateinit var questionText: TextView
+    private lateinit var answer1: RadioButton
+    private lateinit var answer2: RadioButton
+    private lateinit var answer3: RadioButton
+    private lateinit var answer4: RadioButton
 
-    lateinit var binding: FragmentQuestionBinding
 
     lateinit var currentQuestion: Question
     lateinit var answers: MutableList<String>
 
-    private var questionIndex = 1
+    //private var questionIndex = 1
     private val numQuestions = 3
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,21 +68,86 @@ class QuizFragment : Fragment() {
         view?.apply {
             initializeView(this)
             registerListeners(this)
+            randomizeQuestions()
         }
 
         return view
     }
 
-    private fun registerListeners(view: View) {
-        nextButton.setOnClickListener{
-            val snack = Snackbar.make(it,"Next button pressed", Snackbar.LENGTH_SHORT)
-            snack.show()
-            findNavController().navigate(R.id.action_quizFragment_to_endQuizFragment)
+    private fun initializeView(view: View) {
+        nextButton = view.findViewById(R.id.nextButton)
+        questionText = view.findViewById(R.id.question)
+        answer1 = view.findViewById(R.id.answer1)
+        answer2 = view.findViewById(R.id.answer2)
+        answer3 = view.findViewById(R.id.answer3)
+        answer4 = view.findViewById(R.id.answer4)
+        if (questionIndex + 1 == numQuestions) {
+            nextButton.text = "SUBMIT"
         }
     }
 
-    private fun initializeView(view: View) {
-        nextButton = view.findViewById(R.id.nextButton)
+    private fun processAnswer(it: View?): Boolean {
+        //val result = binding.questionRadioGroup.checkedRadioButtonId
+        //firstButton is selected
+        var answerIndex = 0
+//        when (result) {
+//            R.id.answer2 -> answerIndex = 1
+//            R.id.answer3 -> answerIndex = 2
+//            R.id.answer4 -> answerIndex = 3
+//        }
+        if (answers[answerIndex] == currentQuestion.answers[0]) {
+            return true
+        } else {
+            Toast.makeText(this.activity, "Selected answer is wrong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+    }
+
+    private fun showQuestion() {
+        questionIndex++
+        val index = questionIndex
+        val questionTextStr = "$index. " + currentQuestion.text
+
+        questionText.text = questionTextStr
+        answer1.text = answers[0]
+        answer2.text = answers[1]
+        answer3.text = answers[2]
+        answer4.text = answers[3]
+    }
+
+    private fun setQuestion() {
+        currentQuestion = questions[questionIndex]
+        // randomize the answers into a copy of the array
+        answers = currentQuestion.answers.toMutableList()
+        // and shuffle them
+        answers.shuffle()
+        showQuestion()
+    }
+
+    private fun randomizeQuestions() {
+        questions.shuffle()
+        //questionIndex = 0
+        setQuestion()
+    }
+
+    private fun registerListeners(view: View) {
+        nextButton.setOnClickListener {
+            val snack = Snackbar.make(it, "Next button pressed", Snackbar.LENGTH_SHORT)
+            snack.show()
+            //findNavController().navigate(R.id.action_quizFragment_to_endQuizFragment)
+            if (questionIndex < numQuestions) {
+//                Show next question
+                it.findNavController().navigate(R.id.action_quizFragment_self)
+                //setQuestion()
+
+            } else {
+                //End of the test
+                if (questionIndex == numQuestions) {
+                    questionIndex = 0
+                    it.findNavController().navigate(R.id.action_quizFragment_to_endQuizFragment)
+                }
+            }
+        }
     }
 
     companion object {
@@ -86,6 +159,9 @@ class QuizFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment quizFragment.
          */
+
+        var questionIndex = 0
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
