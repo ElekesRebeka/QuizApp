04be4213e3372d97b4dc2ae1116a8e804fc3550c
Elekes Rebeka <elekes.p.rebeka@student.ms.sapientia.ro>
2021-10-27 21:31:56 +0300

    Created menu

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,49 +1,69 @@
 package com.example.quizapp
 
-import android.R.attr
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.database.Cursor
-import android.media.RingtoneManager
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.provider.ContactsContract
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
-import com.google.android.material.snackbar.Snackbar
-import java.lang.Exception
-import java.util.*
-import android.R.attr.data
-import android.text.Editable
-import android.widget.TextView
-import android.R.attr.data
-import java.io.InputStream
-import android.widget.Toast
-import android.R.attr.data
-import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.quizapp.ui.quiz.QuizStartFragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
+
 
 const  val TAG:String = "MainActivity"
 const  val USERNAME_EXTRA:String = "Username"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var playerName: EditText
-    private lateinit var startButton: Button
-    private lateinit var contactButton: Button
+    private lateinit var topAppBar: MaterialToolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i(TAG,"onCreate()")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        initializeView()
-//        registerListeners()
+        initializeView()
+        setSupportActionBar(topAppBar);
+
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        initMenu()
+
+    }
+
+    private fun initializeView() {
+        topAppBar = findViewById(R.id.topAppBar)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+    }
+
+    private fun initMenu(){
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
+        }
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+//            val transaction = supportFragmentManager.beginTransaction()
+//            when(menuItem.itemId) {
+//                //R.id.home -> findNavController(R.id.nav_host_fragment).navigate(R.id.homeFragment)
+//                //R.id.quiz -> findNavController(R.id.nav_host_fragment).navigate(R.id.startFragment)
+//                R.id.home -> transaction.replace(R.id.nav_host_fragment, HomeFragment())
+//                R.id.quiz -> transaction.replace(R.id.nav_host_fragment, QuizStartFragment())
+//
+//            }
+//            transaction.commit()
+
+            when(menuItem.itemId){
+                R.id.home -> findNavController(R.id.nav_host_fragment).navigate(R.id.homeFragment)
+                R.id.quiz -> findNavController(R.id.nav_host_fragment).navigate(R.id.startFragment)
+            }
+
+            drawerLayout.close()
+            true
+        }
     }
 
     override fun onStart() {
